8cb0f256a01eff855b542b7e00bfcfa688d79dac
scogeo <scogeo@users.noreply.github.com>
2023-09-01 18:59:31 +0000

    Updates Package.swift files for release v10.42.1

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@
 import PackageDescription
 
 let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.42.1/Realm.spm.zip"
-let realmChecksum = ""
+let realmChecksum = "5a166d03debda99f4378959a3cf39fc31c1e250a7aa7e9ec90b43450b99f5801"
 
 let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.42.1/RealmSwift@15.0.spm.zip"
-let realmSwiftChecksum = ""
+let realmSwiftChecksum = "65edd34a49cc766a715c5244e80262e82c7e2a0660fd275a9df98def29c12343"
 
 let package = Package(
     name: "realm-swift",
