6f5190459f56bc7aedf9ea20ba9be1076bc43d8d
scogeo <scogeo@users.noreply.github.com>
2023-09-20 00:24:11 +0000

    Updates Package.swift files for release v10.42.3

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -2,11 +2,11 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.42.2/Realm.spm.zip"
-let realmChecksum = ""
+let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.42.3/Realm.spm.zip"
+let realmChecksum = "814d0d58baf6d890fe916db317d58f6be634a0e340ff06f0e4d834e327cbad74"
 
-let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.42.2/RealmSwift@14.3.1.spm.zip"
-let realmSwiftChecksum = ""
+let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.42.3/RealmSwift@14.3.1.spm.zip"
+let realmSwiftChecksum = "7e55713548737eb04daad7fb98431de287b3039bd0c858f08bed6159021fc2ff"
 
 let package = Package(
     name: "realm-swift",
--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -2,11 +2,11 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.42.2/Realm.spm.zip"
-let realmChecksum = ""
+let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.42.3/Realm.spm.zip"
+let realmChecksum = "814d0d58baf6d890fe916db317d58f6be634a0e340ff06f0e4d834e327cbad74"
 
-let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.42.2/RealmSwift@15.0.spm.zip"
-let realmSwiftChecksum = ""
+let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.42.3/RealmSwift@15.0.spm.zip"
+let realmSwiftChecksum = "dceee0c30d6111715f5d97313aedcbb5fb072aca50fefecf1341d826b8f06002"
 
 let package = Package(
     name: "realm-swift",
