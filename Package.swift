1a0c44b8f9c9248a6dba67e7aa04dc57e40f3a23
scogeo <scogeo@users.noreply.github.com>
2023-12-17 00:29:16 +0000

    Updates Package.swift files for release v10.45.0

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.44.0/Realm.spm.zip"
-let realmChecksum = "b26c979ce122acaabb6a6eef9567de43344377afa825c597ae6e14caf508c225"
+let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.45.0/Realm.spm.zip"
+let realmChecksum = "5215a659194877250a3ace4037ff228e217a77cd56af566bcc5f55730654a896"
 
-let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.44.0/RealmSwift@14.3.1.spm.zip"
-let realmSwiftChecksum = "288a311f83ec1d1dc051084f1aeee51ecb165407ef95f5081c9b20df0b952671"
+let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.45.0/RealmSwift@14.3.1.spm.zip"
+let realmSwiftChecksum = "8bd5e4d12080c61044eed665dfbd7cb1ec424547449eb4610fc58419a3571eb5"
 
 let package = Package(
     name: "realm-swift",
--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -2,11 +2,11 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.44.0/Realm.spm.zip"
-let realmChecksum = "b26c979ce122acaabb6a6eef9567de43344377afa825c597ae6e14caf508c225"
+let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.45.0/Realm.spm.zip"
+let realmChecksum = "5215a659194877250a3ace4037ff228e217a77cd56af566bcc5f55730654a896"
 
-let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.44.0/RealmSwift@15.0.spm.zip"
-let realmSwiftChecksum = "c548dd125df800b5888129f93119360cd3bb3b74ebd740bc21a170f411debee1"
+let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.45.0/RealmSwift@15.0.spm.zip"
+let realmSwiftChecksum = "3d88695aae788fe2b08eadf25231561e0a81bc3dbd9a84d732ca6428f215d9e8"
 
 let package = Package(
     name: "realm-swift",
