17b315d31ea3aa730e430d0ed4c94e418cd0bdc9
scogeo <scogeo@users.noreply.github.com>
2024-03-24 00:28:41 +0000

    Updates Package.swift files for release v10.49.1

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -2,11 +2,11 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.49.0/Realm.spm.zip"
-let realmChecksum = "941edfa50340cf7f20a72d0c523f30a7854c82c9bfb4075a7535a70ef29dcd5d"
+let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.49.1/Realm.spm.zip"
+let realmChecksum = "2b7854a2f9372a7d89495cf854c18a9d60b15ed1057afafecd24b6f4281634df"
 
-let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.49.0/RealmSwift@14.3.1.spm.zip"
-let realmSwiftChecksum = "3c4ed66873898550c019720472d2a1523e0e9c9a2ee47dcb43e239ba08d8dd95"
+let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.49.1/RealmSwift@14.3.1.spm.zip"
+let realmSwiftChecksum = "02bc1d7db0b56ea2d1ed4b0b42d0f0b701eef63f2a05ae744a57ee7d4662177e"
 
 let package = Package(
     name: "realm-swift",
--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -2,10 +2,10 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.49.0/Realm.spm.zip"
-let realmChecksum = "941edfa50340cf7f20a72d0c523f30a7854c82c9bfb4075a7535a70ef29dcd5d"
+let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.49.1/Realm.spm.zip"
+let realmChecksum = "2b7854a2f9372a7d89495cf854c18a9d60b15ed1057afafecd24b6f4281634df"
 
-let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.49.0/RealmSwift@15.0.spm.zip"
+let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.49.1/RealmSwift@15.0.spm.zip"
 let realmSwiftChecksum = ""
 
 let package = Package(
