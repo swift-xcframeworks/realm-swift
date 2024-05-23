c0ecdcccf80d2f5ff4b76cf6393f04e1f51a5508
scogeo <scogeo@users.noreply.github.com>
2024-05-23 00:27:07 +0000

    Updates Package.swift files for release v10.50.1

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.50.0/Realm.spm.zip"
-let realmChecksum = "3a2868a488ad715fe62f150110378d48056e927dbc85dfaf45b7084c0136d4f3"
+let realmURL = "https://github.com/realm/realm-swift/releases/download/v10.50.1/Realm.spm.zip"
+let realmChecksum = "942fc39917f4d572d5a2aae6a115c9f50a0954e61351aed3553c84d63ad3f2dc"
 
-let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.50.0/RealmSwift@14.3.1.spm.zip"
+let realmSwiftURL = "https://github.com/realm/realm-swift/releases/download/v10.50.1/RealmSwift@14.3.1.spm.zip"
 let realmSwiftChecksum = ""
 
 let package = Package(
